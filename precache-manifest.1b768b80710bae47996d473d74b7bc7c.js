57a6e41831172d177d1784f3e6f3259dfb655699
Akash Chavda <36260957+skychavda@users.noreply.github.com>
2019-03-11 23:14:48 +0530

    Add files via upload

---
+++ precache-manifest.1b768b80710bae47996d473d74b7bc7c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b3aea7d57a0a8676b5ebcfb774142bf",
+    "url": "./static/media/gallery-3.5b3aea7d.jpg"
+  },
+  {
+    "revision": "ed23a13d5f775a1b24f0",
+    "url": "./static/css/main.94453fbb.chunk.css"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "c96ad4433ad4e29eb518",
+    "url": "./static/js/2.5e10346b.chunk.js"
+  },
+  {
+    "revision": "06592a6963c6ebb980276861b0654171",
+    "url": "./static/media/gallery-8.06592a69.jpg"
+  },
+  {
+    "revision": "8ddec6f9304e9ef1f6fd6e0260bf18f1",
+    "url": "./static/media/gallery-1.8ddec6f9.jpg"
+  },
+  {
+    "revision": "4564aad38b9eb5cf074f5410ee3fdab6",
+    "url": "./static/media/gallery-2.4564aad3.jpg"
+  },
+  {
+    "revision": "ed23a13d5f775a1b24f0",
+    "url": "./static/js/main.79526f62.chunk.js"
+  },
+  {
+    "revision": "7950eff292f3f65dd576a9e889332937",
+    "url": "./static/media/gallery-4.7950eff2.jpg"
+  },
+  {
+    "revision": "14eecfa9b9088edd97853bc254436c10",
+    "url": "./static/media/gallery-5.14eecfa9.jpg"
+  },
+  {
+    "revision": "78ef61d6323f6ab00eb6ec9bc9ebb723",
+    "url": "./static/media/gallery-6.78ef61d6.jpg"
+  },
+  {
+    "revision": "853292e151bb40e13a0c80f427203875",
+    "url": "./static/media/gallery-7.853292e1.jpg"
+  },
+  {
+    "revision": "5a2636903fac8b62d0be589f517f05c0",
+    "url": "./static/media/sofa-2.5a263690.jpg"
+  },
+  {
+    "revision": "3308c53919609a37d373211812cd83d0",
+    "url": "./index.html"
+  }
+];
