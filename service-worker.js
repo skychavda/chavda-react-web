b9abdcd943a0533dfb1c2ebe6d536d7cf33243c3
Akash Chavda <36260957+skychavda@users.noreply.github.com>
2019-03-11 23:22:28 +0530

    feat: mobile

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b768b80710bae47996d473d74b7bc7c.js"
+  "./precache-manifest.a04fcf7e2f08dfe0e9532ea79a6457e6.js"
 );
 
 workbox.clientsClaim();
